467658e88149371fe3616f62e07168a738603f06
Gurudasnov <svt.subscribe@gmail.com>
2019-11-08 09:57:52 +0530

    Add files via upload

---
+++ assignments/harika/PropertiesTest1.java
@@ -0,0 +1,53 @@
+package assignments.harika;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+
+
+public class PropertiesTest1 {     
+    public static void main(String[] args) {
+    	HashMap<String, String> map=new HashMap<String,String>();
+    	
+   
+		try {
+             BufferedReader reader;
+             reader = new BufferedReader(new FileReader(args[0]));
+             String line;       
+            
+            while ((line = reader.readLine()) != null) {
+                String[] arrOfStr = line.split("=", 2); 
+                map.put(arrOfStr[0],arrOfStr[1]); 
+            }
+            System.out.println("The keys and values in the file without duplicates\n"+map);
+            System.out.println("The Values are: \n");
+            Collection<String> values=map.values();
+            for(String value:values)
+            {
+            	System.out.println(value);
+            }
+            System.out.println("The keys are : \n");
+            Set<String> keys = map.keySet();
+            for(String key:keys)
+            {
+            	System.out.println(key);
+            }
+            System.out.println("The Keys and Values are : \n");
+            Set<java.util.Map.Entry<String,String>> entry=map.entrySet();
+            for(java.util.Map.Entry<String, String>  ee:entry)
+            {
+            	System.out.println(ee.getKey()+ " : " +ee.getValue());
+            }
+            
+           
+        }
+        catch (IOException e) {
+             e.printStackTrace();
+             }
+     }
+}
+
