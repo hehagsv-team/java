8d50176df05570af51bda9f2ff0a8b4fb2e301e7
Gurudasnov <svt.subscribe@gmail.com>
2019-11-08 10:20:06 +0530

    Add files via upload
    ver 2

--- assignments/varun/BODMASTest1.java
+++ assignments/varun/BODMASTest1.java
@@ -1,110 +1,321 @@
 package assignments.varun;
 
-
 import java.util.Stack;
 
-public class BODMASTest1
-{
-    public int evaluate(String expr)
-    {
-        char[] splitString = expr.toCharArray();
+public class BODMASTest1 {
+	
+	static void evalution(StringBuffer substring)
+	{
+		int lastOpenBrace=substring.lastIndexOf("(");   
+		int firstCloseBrace=substring.indexOf(")");
+		if(firstCloseBrace==-1 && lastOpenBrace==-1)
+		{	String string=substring.toString();
+			char[] returned=calculateExpression(string);
+			StringBuffer sb=new StringBuffer();
+			String SpacelessString=String.valueOf(returned).trim();
+			System.out.println(SpacelessString);
+		}
+		else
+		{
+			
+			String cal=substring.substring(lastOpenBrace+1,firstCloseBrace);
+			char[] returned=calculateExpression(cal);
+			StringBuffer sb=new StringBuffer();
+			String str=String.valueOf(returned).trim();
+			sb=substring.replace(lastOpenBrace, firstCloseBrace+1, str);
+			try
+			{
 
-        Stack<Integer> numbers = new Stack<Integer>();
-        Stack<Character> operators = new Stack<Character>();
+				evalution(sb);
+       		 
+			}
+			catch(Exception e)
+			{
+				System.out.println("Please give the braces properly in the expression");
+				System.exit(0);
+			}
+		}
+		
 
-        for (int i = 0; i < splitString.length; i++)
+	}
+
+	private static char[] calculateExpression(String cal) {
+		Stack<Integer> integers=new Stack<Integer>();
+		Stack<Character> operators=new Stack<Character>();
+		char[] splitString=cal.toCharArray();
+		//System.out.println(splitString);
+		int lowForSub;
+		int lowForDivison,highForDivision;
+		int lowForMultiplication,highforMultiplication;
+		int lowForAdd,highForAdd;
+		int highForSub,sr,sh_s;//sh_s---->shift start
+        for(int i=0;i<splitString.length;i++)
         {
-            // ignore space
-        	if (Character.isWhitespace(splitString[i]))
-        		continue;
-//        	if (splitString[i] == ' ')
-//                continue;
+        	for(int div=0;div<splitString.length;div++)
+        	{
+        		if(splitString[div]=='/')
+        		{
+        			StringBuffer num = new StringBuffer();
+        			for(lowForDivison=div-1;lowForDivison>=0;lowForDivison--)
+        			{
+        				if(splitString[lowForDivison]>='0' && splitString[lowForDivison]<='9')
+        					num.append(splitString[lowForDivison]);
+        				else
+        					break;
+        			}
+        			num.reverse();
+        			try
+        			{
 
-        	// check for negative sign followed by digits but it should not be minus operator
-        	// define a negative number:
-        	// 1. there should not be digit before the negative sign
-        	// 2. there should not be an operation before the negative sign
+               			integers.push(Integer.parseInt(num.toString()));
+               		 
+        			}
+        			catch(Exception e)
+        			{
+        				System.out.println("Please do not enter the operator followed by operator");
+        				System.exit(0);
+        			}
+        			num.delete(0, num.length());
+        			for(highForDivision=div+1;highForDivision<splitString.length;highForDivision++)
+        			{
+        				if(splitString[highForDivision]>='0' && splitString[highForDivision]<='9')
+        					num.append(splitString[highForDivision]);
+        				else
+        					break;
+        			}
+        			integers.push(Integer.parseInt(num.toString()));
+        			num.delete(0, num.length());
+        			int value=calculate(integers.pop(),integers.pop(),splitString[div]);
+        			String c=Integer.toString(value);
+        			char[] split2=c.toCharArray();
+        			for(sr=0;sr<split2.length;sr++)
+        			{
+        				sh_s=lowForDivison+sr+1;
+        				splitString[sh_s]=split2[sr];
+        			}
+        			int repetetionForShift=(highForDivision-1)-(lowForDivison+split2.length);
+        			int backup=repetetionForShift;
+        			while(repetetionForShift>0)
+        			{
+        				shiftleft(lowForDivison+split2.length+1,splitString.length,splitString);
+        				repetetionForShift--;
+        			}
+        			//make last 2 elements null
+        			for(int it=splitString.length-1;it>=(splitString.length-backup);it--)
+        			{
+        				splitString[it]='\0';
+        			}
+        		}	
+        	}
+        	integers.clear();
+        	//for multiplication
+        	for(int mul=0;mul<splitString.length;mul++)
+        	{
+        		if(splitString[mul]=='*')
+        		{
+        			StringBuffer num = new StringBuffer();
+        			for(lowForMultiplication=mul-1;lowForMultiplication>=0;lowForMultiplication--)
+        			{
+        				if(splitString[lowForMultiplication]>='0' && splitString[lowForMultiplication]<='9')
+        					num.append(splitString[lowForMultiplication]);
+        				else
+        					break;
+        			}
+        			num.reverse();
+        			try
+        			{
+
+               			integers.push(Integer.parseInt(num.toString()));
+               		 
+        			}
+        			catch(Exception e)
+        			{
+        				System.out.println("Please do not enter the operator followed by operator");
+        				System.exit(0);
+        			}
+        			num.delete(0, num.length());
+        			for(highforMultiplication=mul+1;highforMultiplication<splitString.length;highforMultiplication++)
+        			{
+        				if(splitString[highforMultiplication]>='0' && splitString[highforMultiplication]<='9')
+        					num.append(splitString[highforMultiplication]);
+        				else
+        					break;
+        			}
+        			integers.push(Integer.parseInt(num.toString()));
+        			num.delete(0, num.length());
+        			int value=calculate(integers.pop(),integers.pop(),splitString[mul]);
+        			String c1=Integer.toString(value);
+        			char[] splitForMul=c1.toCharArray();
+        			for(sr=0;sr<splitForMul.length;sr++)
+        			{
+        				sh_s=lowForMultiplication+sr+1;
+        				splitString[sh_s]=splitForMul[sr];
+        			}
+        			int shiftRepForMul=(highforMultiplication-1)-(lowForMultiplication+splitForMul.length);
+        			int backupForMUL=shiftRepForMul;
+        			while(shiftRepForMul>0)
+        			{
+        				shiftleft(lowForMultiplication+splitForMul.length+1,splitString.length,splitString);
+        				shiftRepForMul--;
+        			}
+        			//make last 2 elements null
+        			for(int it=splitString.length-1;it>=(splitString.length-backupForMUL);it--)
+        			{
+        				splitString[it]='\0';
+        			}
+        		}	
+        	}
+        	//for addition 
+        	for(int add=0;add<splitString.length;add++)
+        	{
+        		if(splitString[add]=='+')
+        		{
+        			StringBuffer num = new StringBuffer();
+        			for(lowForAdd=add-1;lowForAdd>=0;lowForAdd--)
+        			{
+        				if(splitString[lowForAdd]>='0' && splitString[lowForAdd]<='9')
+        					num.append(splitString[lowForAdd]);
+        				else
+        					break;
+        			}
+        			num.reverse();
+        			try
+        			{
+
+               			integers.push(Integer.parseInt(num.toString()));
+               		 
+        			}
+        			catch(Exception e)
+        			{
+        				System.out.println("Please do not enter the operator followed by operator");
+        				System.exit(0);
+        			}
+        			num.delete(0, num.length());
+        			for(highForAdd=add+1;highForAdd<splitString.length;highForAdd++)
+        			{
+        				if(splitString[highForAdd]>='0' && splitString[highForAdd]<='9')
+        					num.append(splitString[highForAdd]);
+        				else
+        					break;
+        			}
+        			integers.push(Integer.parseInt(num.toString()));
+        			num.delete(0, num.length());
+        			int f1Add=calculate(integers.pop(),integers.pop(),splitString[add]);
+        			String c1Add=Integer.toString(f1Add);
+        			char[] splitForAdd=c1Add.toCharArray();
+        			for(sr=0;sr<splitForAdd.length;sr++)
+        			{
+        				sh_s=lowForAdd+sr+1;
+        				splitString[sh_s]=splitForAdd[sr];
+        			}
+        			int shiftRepForAdd=(highForAdd-1)-(lowForAdd+splitForAdd.length);
+        			int count=0;
+        			int backupForAdd=shiftRepForAdd;
+        			while(shiftRepForAdd>0)
+        			{
+        				shiftleft(lowForAdd+splitForAdd.length+1,splitString.length,splitString);
+        				shiftRepForAdd--;
+        				count++;
+        			}
+        			//make last 2 elements null
+        			for(int it=splitString.length-1;it>=(splitString.length-backupForAdd);it--)
+        			{
+        				splitString[it]='\0';
+        			}
+        		}	
+        	}
+        	//for substraction
+        	for(int sub=0;sub<splitString.length;sub++)
+        	{
+        		if(splitString[sub]=='-')
+        		{
+        			StringBuffer num = new StringBuffer();
+        			for(lowForSub=sub-1;lowForSub>=0;lowForSub--)
+        			{
+        				if(splitString[lowForSub]>='0' && splitString[lowForSub]<='9')
+        					num.append(splitString[lowForSub]);
+        				else
+        					break;
+        			}
+        			num.reverse();
+        			try
+        			{
+
+               			integers.push(Integer.parseInt(num.toString()));
+               		 
+        			}
+        			catch(Exception e)
+        			{
+        				System.out.println("Please do not enter the operator followed by operator");
+        				System.exit(0);
+        			}
+        			num.delete(0, num.length());
+        			for(highForSub=sub+1;highForSub<splitString.length;highForSub++)
+        			{
+        				if(splitString[highForSub]>='0' && splitString[highForSub]<='9')
+        					num.append(splitString[highForSub]);
+        				else
+        					break;
+        			}
+        			integers.push(Integer.parseInt(num.toString()));
+        			num.delete(0, num.length());
+        			int f1Sub=calculate(integers.pop(),integers.pop(),splitString[sub]);
+        			String c1Sub=Integer.toString(f1Sub);
+        			char[] splitForSub=c1Sub.toCharArray();
+        			for(sr=0;sr<splitForSub.length;sr++)
+        			{
+        				sh_s=lowForSub+sr+1;
+        				splitString[sh_s]=splitForSub[sr];
+        			}
+        			int shiftRepForSub=(highForSub-1)-(lowForSub+splitForSub.length);
+        			int count=0;
+        			int backupForSub=shiftRepForSub;
+        			while(shiftRepForSub>0)
+        			{
+        				shiftleft(lowForSub+splitForSub.length+1,splitString.length,splitString);
+        				shiftRepForSub--;
+        				count++;
+        			}
+        			//make last 2 elements null
+        			for(int it=splitString.length-1;it>=(splitString.length-backupForSub);it--)
+        			{
+        				splitString[it]='\0';
+        			}
+        		}	
+        	}
         	
-        	// if digits and the following numbers are digits
-        	if (Character.isDigit(splitString[i]))
-//            if (splitString[i] >= '0' && splitString[i] <= '9')
-            {
-        		// return the entire number 
-//        		int localNumber = getNumber (splitString, i);
-        		
-                StringBuffer numberString = new StringBuffer();
-                while (i < splitString.length && splitString[i] >= '0' && splitString[i] <= '9')
-                    numberString.append(splitString[i++]);
-                numbers.push(Integer.parseInt(numberString.toString()));
-            }
+        	
+        }	
+        return splitString;	
+	}
 
-            /**
-             * brackets take precedence.
-             * push all operations within a bracket into a stack
-             * a bracket within a bracket takes more precedence.
-             */
-            
-            else if (splitString[i] == '(')
-                operators.push(splitString[i]);
+	private static void shiftleft(int start, int length, char[] splitString) {
+		
+		for(int starts=start;starts<splitString.length-1;starts++)   //start_i--->s_i
+		{
+			splitString[starts]=splitString[starts+1];
+		}
 
-            else if (splitString[i] == ')')
-            {
-                while (operators.peek() != '(')
-                  numbers.push(calculate(operators.pop(), numbers.pop(), numbers.pop()));
-                operators.pop();
-            }
+	}
 
-            else if (splitString[i] == '+' || splitString[i] == '-' ||
-                     splitString[i] == '*' || splitString[i] == '/')
-            {
-                
-                while (!operators.empty() && checkPrecedence(splitString[i], operators.peek()))
-                  numbers.push(calculate(operators.pop(), numbers.pop(), numbers.pop()));
+	private static int calculate(Integer num1, Integer num2, char operand) {
+		switch(operand)
+		{
+		case '/':
+			return num2/num1;
+		case '*':
+			return num2*num1;
+		case '+':
+			return num2+num1;
+		case '-':
+			return num2-num1;
+		}
+		return 0;
+		
+	}
 
-                
-                operators.push(splitString[i]);
-            }
-        }
-
-        while (!operators.empty())
-            numbers.push(calculate(operators.pop(), numbers.pop(), numbers.pop()));
-
-        
-        return numbers.pop();
-    }
-
-    public boolean checkPrecedence(char op1, char op2)
-    {
-        if (op2 == '(' || op2 == ')')
-            return false;
-        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
-            return false;
-        else
-            return true;
-    }
-
-    public int calculate(char op, int b, int a)
-    {
-        switch (op)
-        {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
-        case '/':
-            if (b == 0)
-                throw new
-                UnsupportedOperationException("Cannot divide by zero");
-            return a / b;
-        }
-        return 0;
-    }
-
-    public static void main(String[] args)
-    {
-        BODMASTest1 test = new BODMASTest1(); 
-    	System.out.println(test.evaluate("-12 * ( 2+ (-12*2) ) / 14"));
-    }
-}
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer("12/2*6+9/5(14+22+(22+33+(44/2*22-2+444/2)))");
+		evalution(sb);
+		}
+	}
