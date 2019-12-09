8814f58fe80cbc4b3ef7ee239fb8d4f3cbf7afba
jsmounika <49757223+jsmounika@users.noreply.github.com>
2019-12-09 11:34:00 +0530

    Add files via upload

---
+++ assignments/Mounika/QueryJDBC.java
@@ -0,0 +1,30 @@
+class Purchase{
+	 
+	    Connection conn;
+		Statement smt;
+		public Purchase(Connection conn, Statement smt)
+		{
+			this.conn=conn;
+			this.smt=smt;
+		}
+		
+		void purchaseOrder() {
+		
+			try {		
+
+					String sql1 ="UPDATE hcl_sk_order SET payment=1 WHERE id=200";							
+					String sql2 ="INSERT into HCL_SK_SHIPPING_ORDER(id,order_id) SELECT item_id,id FROM hcl_sk_order WHERE id=200";
+				    
+					smt.executeUpdate(sql1);
+					smt.executeUpdate(sql2);
+					System.out.println("Purchase of order is done");	
+		
+			     
+			}
+			catch(Exception ex) 
+			{ 
+				System.err.println(ex); 
+			} 
+		} 
+	 
+ }
