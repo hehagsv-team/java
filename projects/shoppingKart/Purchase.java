de31d6b04651e7299daf5bd31639fc1a2459b8c1
Varun Reddi <37947487+varunreddi@users.noreply.github.com>
2019-12-12 15:27:48 +0530

    Update Purchase.java

--- projects/shoppingKart/Purchase.java
+++ projects/shoppingKart/Purchase.java
@@ -1,36 +1,74 @@
 package projects.shoppingKart;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Purchase{
-	 
-	    Connection conn;
-		Statement smt;
-		public Purchase (Connection conn, Statement smt)
-		{
-			this.conn=conn;
-			this.smt=smt;
-		}
-		
-		void purchaseOrder() {
-		
-			try {		
+class Purchase {
 
-					String sql1 ="UPDATE hcl_sk_order SET payment=1 WHERE id=200";							
-//					String sql2 ="INSERT into HCL_SK_SHIPPING_ORDER(id,order_id) SELECT item_id,id FROM hcl_sk_order WHERE id=200";
-//				    
-					smt.executeUpdate(sql1);
-//					smt.executeUpdate(sql2);
-					System.out.println("Purchase of order is done");	
+	ResultSet resultSet;
+
+	public boolean purchaseOrder(int order_id, Statement statement) {	
 		
-			     
-			}
-			catch(Exception ex) 
-			{ 
-				System.err.println(ex); 
+		try {
+			String sql1 = "UPDATE hcl_sk_order SET payment=1 WHERE id=" + order_id;
+			System.out.println(sql1);
+			int result;
+			result = statement.executeUpdate(sql1);
+			if (result > 0) {
+				System.out.println("Update of payment is done successfully");
+				return true;
 			} 
-		} 
-	 
- }
+		} catch (SQLException e) {
+			System.out.println("error in updating payment");
+//			e.printStackTrace();
+		}
+
+		
+		int result1;
+		try {
+			String sql2 = "INSERT into HCL_SK_SHIPPING_ORDER(id,order_id) values(HCL_SK_SHIPPING_ORDER_ID_SEQ.nextval,"+ order_id + ")";
+			System.out.println(sql2);
+			result1 = statement.executeUpdate(sql2);
+			if (result1 > 0) {
+				System.out.println("Insert of Shipping details is done successfully");
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println("error in inserting shipping details");
+//			e.printStackTrace();
+		}
+
+		
+		return false;
+	}
+
+	public ArrayList getOrderDetails(int order_id, Statement statement) {
+		try {
+				String executeQuery = "select item_id,id,order_date,Quantity from hcl_sk_order where id=" + order_id;
+				ResultSet resultSet = statement.executeQuery(executeQuery);
+				ArrayList list = new ArrayList();
+
+				while (resultSet.next()) {
+					list.add(resultSet.getInt("item_id"));
+					list.add(resultSet.getInt("id"));				
+					list.add(resultSet.getDate("order_date"));
+					list.add(resultSet.getInt("quantity"));
+				}
+				int size = list.size();
+				if (size == 0)
+					return null;
+				return list;
+
+		} catch (SQLException e) {
+
+			System.out.println("Error in selecting the Order Details");
+			e.printStackTrace();
+
+			return null;
+		}
+	}
+
+}
+
+
