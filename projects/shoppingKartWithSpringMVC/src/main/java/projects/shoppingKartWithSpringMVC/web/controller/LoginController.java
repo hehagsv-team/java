b85b3b5de0964d316a3f2d4290f69898e34c5240
Gurudasnov <svt.subscribe@gmail.com>
2020-01-23 13:37:59 +0530

    Add files via upload

--- projects/shoppingKartWithSpringMVC/src/main/java/projects/shoppingKartWithSpringMVC/web/controller/LoginController.java
+++ projects/shoppingKartWithSpringMVC/src/main/java/projects/shoppingKartWithSpringMVC/web/controller/LoginController.java
@@ -1,5 +1,4 @@
 package projects.shoppingKartWithSpringMVC.web.controller;
-//package com.in28minutes.springboot.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.in28minutes.springboot.web.service.LoginService;
+import projects.shoppingKartWithSpringMVC.web.service.LoginService;
 @Controller
 public class LoginController 
 {
