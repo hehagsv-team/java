79084c2ed6429ad57c7f92df39be7613a0a5fb82
hemadrit <49308295+hemadrit@users.noreply.github.com>
2020-01-28 11:47:40 +0530

    Add files via upload

--- projects/shoppingKartWithSpringMVC/src/main/java/projects/shoppingKartWithSpringMVC/web/controller/LoginController.java
+++ projects/shoppingKartWithSpringMVC/src/main/java/projects/shoppingKartWithSpringMVC/web/controller/LoginController.java
@@ -1,5 +1,7 @@
 package projects.shoppingKartWithSpringMVC.web.controller;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
 import projects.shoppingKartWithSpringMVC.web.service.LoginService;
 @Controller
 public class LoginController 
@@ -28,6 +33,18 @@ public class LoginController
     		   model.put("errorMessage", "Username doesnot exist");
     		   return "login";
     	   }
+     	   model.put("name",name);
+ 	        return "welcome";
+       }
+       @RequestMapping(value="/NewUser",method=RequestMethod.GET)
+       public String showNewUserLoginPage(ModelMap model)
+       {  	   
+//    	   model.put("name",name);
+ 	        return "NewUser";
+       }
+       @RequestMapping(value="/NewUser",method=RequestMethod.POST)
+       public String showNewUserWelcomePage(ModelMap model,@RequestParam String name)
+       {  	   
      	   model.put("name",name);
  	        return "welcome";
        }
