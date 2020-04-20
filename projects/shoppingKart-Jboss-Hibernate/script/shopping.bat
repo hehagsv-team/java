68a92cfa6aa423d3404ab7c21881bcd7ea844cd8
jsmounika <49757223+jsmounika@users.noreply.github.com>
2020-04-20 13:13:44 +0530

    Update shopping.bat

--- projects/shoppingKart-Jboss-Hibernate/script/shopping.bat
+++ projects/shoppingKart-Jboss-Hibernate/script/shopping.bat
@@ -1,6 +1,6 @@
 
 @ECHO OFF
-REM Keep the following attributes in standalone.xml at deployment-scanner element 
+REM Keep the following attributes in standalone.xml(wildfly-10.0.0.Final\standalone\configuration\standalone.xml) at deployment-scanner element 
 REM  <deployment-scanner path="deployments" relative-to="jboss.server.base.dir" scan-enabled="true" scan-interval="5000" auto-deploy-exploded="true" auto-deploy-zipped="true"  runtime-failure-causes-rollback="${jboss.deployment.scanner.rollback.on.failure:false}"/>
 cd c:\
 REM go to the directory where source and pom.xml present
