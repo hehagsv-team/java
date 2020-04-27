1e798de7de51490175192caf8122d0f33234de75
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-04-27 18:39:15 +0530

    Add files via upload

---
+++ assignments/sandhya/src/jdbc/schoolschema/ClassStrength.java
@@ -0,0 +1,59 @@
+package jdbc.schoolschema;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class ClassStrength {	
+	static String sql;
+public static String classStrength() {
+	return sql = "select count(*)\r\n" + 
+ 	 		"from student s\r\n" + 
+ 	 		"join class c\r\n" + 
+ 	 		"on s.class_name=c.name\r\n" +
+ 	 	    "where c.name=?\r\n";
+}
+public static void classStrengthMethod(Scanner sc, Connection conn) throws SQLException {
+	while(true) {
+	  	  System.out.println("\nClass strength");
+		  Menu.classMenu();
+  	      int ch=sc.nextInt();
+  	      sql=classStrength(); 
+  	      ResultSet rs=null;
+	      PreparedStatement stmt=conn.prepareStatement(sql);
+		  subClassMethod(rs,stmt,ch);
+		  if(ch==12) {
+			  break;
+		    }
+         }
+}
+private static void subClassMethod(ResultSet rs, PreparedStatement stmt, int ch) throws SQLException {
+	switch(ch) {
+	  case 1:stmt.setInt(1,6);
+             rs = stmt.executeQuery();
+             getClassStrength(rs,6); break;
+      case 2:stmt.setInt(1,7);
+      		rs = stmt.executeQuery();
+            getClassStrength(rs,7); break;
+	  case 3:stmt.setInt(1,8);
+            rs = stmt.executeQuery();
+            getClassStrength(rs,8); break;
+	  case 4:stmt.setInt(1,9);
+             rs = stmt.executeQuery();
+             getClassStrength(rs,9); break;
+	  case 5:stmt.setInt(1,10);
+            rs = stmt.executeQuery();
+            getClassStrength(rs,10); break;
+      default: break;
+	  }
+	
+}
+private static void getClassStrength(ResultSet rs, int i) throws SQLException {
+	while(rs.next()) {
+	    System.out.print("The strength of the class "+i+" is "+rs.getInt("count(*)")+"\n");
+	}
+	}
+
+}
---
+++ assignments/sandhya/src/jdbc/schoolschema/ClassTeacherClass.java
@@ -0,0 +1,63 @@
+package jdbc.schoolschema;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class ClassTeacherClass {
+	static String sql;
+	public static String classTeacherSub() {
+		return sql = "select t.name,s.name as no_subject\r\n" + 
+	 	 		"from subject s\r\n" + 
+	 	 		"join teacher t\r\n" + 
+	 	 		"on s.teacher_id=t.id\r\n" + 
+	 	 		"join class c\r\n" + 
+	 	 		"on c.teacher_id=t.id\r\n" + 
+	 	 	    "where c.name=?\r\n"+
+	 	 		"group by t.name,s.name\r\n";
+	}
+	public static void classTeacherMethod(Scanner sc, Connection conn) throws SQLException {
+	      while(true) {
+	      System.out.println("\nWhich all subjects does a class teacher teach is");
+		  Menu.classMenu();
+		  int ch=sc.nextInt();
+		  sql=classTeacherSub();
+		  ResultSet rs=null;
+	      PreparedStatement stmt=conn.prepareStatement(sql);
+		  subClassTeacher(ch,stmt,rs);
+		  if(ch==12) {
+			  break;
+		    }
+	      }
+	}
+	private static void subClassTeacher(int ch, PreparedStatement stmt, ResultSet rs) throws SQLException {
+		 switch(ch) {
+		 case 1:stmt.setInt(1,6);
+	          rs = stmt.executeQuery();
+	          getClassTeacher(rs,6);  break;
+		  case 2:stmt.setInt(1,7);
+	          rs = stmt.executeQuery();
+	          getClassTeacher(rs,7);  break;
+		  case 3:stmt.setInt(1,8);
+	          rs = stmt.executeQuery();
+	          getClassTeacher(rs,8);  break;
+		  case 4:stmt.setInt(1,9);
+	          rs = stmt.executeQuery();
+	          getClassTeacher(rs,9);  break;
+		  case 5:stmt.setInt(1,10);
+	          rs = stmt.executeQuery();
+	          getClassTeacher(rs,10);
+	          System.out.println("null");  break;
+	      default:  break;
+		  }
+	}
+	private static void getClassTeacher(ResultSet rs, int i) throws SQLException {
+		System.out.println("The class teacher of the class name "+i+" is:");
+		while(rs.next()) {
+			    System.out.print("Classteacher name: "+rs.getString("name")+", Subject name: "+rs.getString("no_subject"));
+			    System.out.println();
+			}
+		}
+}
