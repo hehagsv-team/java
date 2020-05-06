b364fdfe0886b149e0114a7f984609ea2404116c
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-06 13:50:00 +0530

    Add files via upload

---
+++ assignments/sandhya/TestSchool- 6-5-2020/Menu.java
@@ -0,0 +1,12 @@
+
+public class Menu {
+	public static void classMenu() {
+		System.out.println("Enter the class name you need:");
+		System.out.println("1: class name 6: ");
+		System.out.println("2: class name 7: ");
+		System.out.println("3: class name 8: ");
+		System.out.println("4: class name 9: ");
+		System.out.println("5: class name 10: ");
+	    System.out.println("12: To return main menu:");
+	}
+}
