e84035c2f3a7f93d4defc5a6cfa5a89b42890a94
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-07 13:44:38 +0530

    Update SubjectTopperClassTest.java

--- assignments/sandhya/src/test/java/com/schoolschema/Testjunit/schoolschema/SubjectTopperClassTest.java
+++ assignments/sandhya/src/test/java/com/schoolschema/Testjunit/schoolschema/SubjectTopperClassTest.java
@@ -30,7 +30,7 @@ public void setUp() throws Exception {
 	when(stmt.executeQuery()).thenReturn(rs);	
 }
 @Test
-public void test1() throws Exception{
+public void subjectTopper_Case1() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=1;
 	when(rs.next()).thenReturn(true);
@@ -41,7 +41,7 @@ public void test1() throws Exception{
 	assertEquals("Physics-Anu-96",result);
 }
 @Test
-public void test2() throws Exception{
+public void subjectTopper_Case2() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=2;
 	when(rs.next()).thenReturn(true);
@@ -52,7 +52,7 @@ public void test2() throws Exception{
 	assertEquals("Chemistry-laila-93",result);
 }
 @Test
-public void test3() throws Exception{
+public void subjectTopper_Case3() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=3;
 	when(rs.next()).thenReturn(true);
@@ -63,7 +63,7 @@ public void test3() throws Exception{
 	assertEquals("BIOLOGY-san-90",result);
 }
 @Test
-public void test4() throws Exception{
+public void subjectTopper_Case4() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=4;
 	when(rs.next()).thenReturn(true);
@@ -74,7 +74,7 @@ public void test4() throws Exception{
 	assertEquals("HISTORY-hemadri-94",result);
 }
 @Test
-public void test5() throws Exception{
+public void subjectTopper_Case5() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=5;
 	when(rs.next()).thenReturn(true);
@@ -85,7 +85,7 @@ public void test5() throws Exception{
 	assertEquals("GEOGRAPHY-Varun-92",result);
 }
 @Test
-public void test6() throws Exception{
+public void subjectTopper_Case6() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=6;
 	when(rs.next()).thenReturn(true);
@@ -96,7 +96,7 @@ public void test6() throws Exception{
 	assertEquals("POLITICAL SCIENCE-Harika-93",result);
 }
 @Test
-public void test7() throws Exception{
+public void subjectTopper_Case7() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=7;
 	when(rs.next()).thenReturn(true);
@@ -107,7 +107,7 @@ public void test7() throws Exception{
 	assertEquals("ECONOMICS-Sony-90",result);
 }
 @Test
-public void test8() throws Exception{
+public void subjectTopper_Case8() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=8;
 	when(rs.next()).thenReturn(true);
@@ -118,7 +118,7 @@ public void test8() throws Exception{
 	assertEquals("ENGLISH-Gowthu-91",result);
 }
 @Test
-public void test9() throws Exception{
+public void subjectTopper_Case9() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=9;
 	when(rs.next()).thenReturn(true);
@@ -129,7 +129,7 @@ public void test9() throws Exception{
 	assertEquals("HINDI-Anvar-85",result);
 }
 @Test
-public void invalidBoundaryTestSubject() throws Exception{
+public void invalidBoundaryTest_subjectTopper() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=0;
 	String result=SubjectTopperClass.subjectTopper(stmt, ch);
@@ -137,21 +137,21 @@ public void invalidBoundaryTestSubject() throws Exception{
 	}
 
 @Test
-public void validBoundaryTestSubject() throws Exception{
+public void validBoundaryTest_subjectTopper() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	int ch=10;
 	String result=SubjectTopperClass.subjectTopper(stmt, ch);
 	assertEquals("valid",result);
 	}
 @Test
-public void testWhileExitMain() throws Exception{
+public void whileExitMain() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	Scanner sc=new Scanner("12");
 	String result=SubjectTopperClass.subjectTopperMethod(sc,conn);
 	assertEquals("exit",result);
 }
 @Test
-public void testWhileExit() throws Exception{
+public void whileExit_subTopperMethod() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	Scanner sc=new Scanner("11");
 	int ch=2;
@@ -169,7 +169,7 @@ public void testWhileExit() throws Exception{
 
 }
 @Test
-public void testValidBoundaryConditionClass() throws Exception{
+public void validBoundaryCondition_SubTopperMethod() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	Scanner sc=new Scanner("11");
 	int ch=1;
@@ -177,15 +177,12 @@ public void testValidBoundaryConditionClass() throws Exception{
 	assertEquals("valid",result);
 }
 @Test
-public void testInvalidBoundaryConditionClass() throws Exception{
+public void invalidBoundaryCondition_SubTopperMethod() throws Exception{
 	SubjectTopperClass stc=new SubjectTopperClass();
 	Scanner sc=new Scanner("11");
 	int ch=11;
 	String result=SubjectTopperClass.subTopperMethod(ch,stmt,sc);
 	assertEquals("invalid",result);
 }
-
-
 }
-
 
