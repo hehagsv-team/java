2bab721334ef6a7046d04e4364a3d8d77d1a0e94
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-07 09:37:33 +0530

    Update SubjectTopperClass.java

--- assignments/sandhya/src/main/java/com/schoolschema/Testjunit/schoolschema/SubjectTopperClass.java
+++ assignments/sandhya/src/main/java/com/schoolschema/Testjunit/schoolschema/SubjectTopperClass.java
@@ -38,48 +38,55 @@ public class SubjectTopperClass {
 		return res;
 		
 	}
-	private static void subTopperMethod(int ch, PreparedStatement stmt, Scanner sc) throws SQLException {
+	public static String subTopperMethod(int ch, PreparedStatement stmt, Scanner sc) throws SQLException {
 		String res="";
 		switch(ch) {
 		  case 1:while(true) {
 			  ch=subjectMethod(sc);
 			  stmt.setInt(1,6);
 			  res=subjectTopper(stmt,ch);
-			  if(ch==11) break;
+			  if(ch==11) {
+				 res="valid"; break;
+			  }
 			  }break;
 		  case 2:while(true) {
 			  ch=subjectMethod(sc);
 			  stmt.setInt(1,7);
 			  subjectTopper(stmt,ch);
-			  if(ch==11) break;
-			  }break;
+			  if(ch==11) {
+					 res="exit"; break;
+				 }}break;
 		  case 3:while(true) {
 			  ch=subjectMethod(sc);
 			  stmt.setInt(1,8);
 			  subjectTopper(stmt,ch);
-			  if(ch==11) break;
-			  }break;
+			  if(ch==11) {
+					 res="exit"; break;
+				 }}break;
 		  case 4:while(true) {
 			  ch=subjectMethod(sc);
 			  stmt.setInt(1,9);
 			  subjectTopper(stmt,ch);
-			  if(ch==11) break;
-			  }break;
+			  if(ch==11) {
+					 res="exit"; break;
+				 }}break;
 		  case 5:while(true) {
 			  ch=subjectMethod(sc);
 			  stmt.setInt(1,10);
 			  subjectTopper(stmt,ch);
-			  if(ch==11) break;
-		       }break;
-	      default:break;
+			  if(ch==11) {
+					 res="exit"; break;
+				 }}break;
+	      default:res="invalid";break;
 		}
+		return res;
 		  }
 	
-	private static int subjectMethod(Scanner sc) {
+	public static int subjectMethod(Scanner sc) {
 			  Menu.subjectMenu();
 			  System.out.println("11: To return main menu:");
 			  int ch;
-	  		  if(sc.hasNext()) {
+			  if(sc.hasNext()) {
 		  	    	ch=Integer.valueOf(sc.nextLine()).intValue();
 				
 		  	    } 
@@ -89,6 +96,58 @@ public class SubjectTopperClass {
 			  return ch;
 			}
 	
+	public static String subjectTopper(PreparedStatement stmt, int ch) throws SQLException {
+		String res="";
+		ResultSet rs=null;
+	  	  switch(ch) {
+	  	  case 1:stmt.setString(2,"PHYSICS");
+	      	  	res=getSubjectTopper(stmt,rs);break;
+	  	  case 2:stmt.setString(2,"CHEMISTRY");
+	  	  		 res=getSubjectTopper(stmt,rs);break;
+	  	  case 3:stmt.setString(2,"BIOLOGY");
+	  	         res=getSubjectTopper(stmt,rs);break; 
+	  	  case 4:stmt.setString(2,"HISTORY");
+	  	         res=getSubjectTopper(stmt,rs);break; 
+	  	  case 5:stmt.setString(2,"GEOGRAPHY");
+	  	         res=getSubjectTopper(stmt,rs);break;
+	  	  case 6:stmt.setString(2,"POLITICAL SCIENCE");
+	  	         res=getSubjectTopper(stmt,rs);break; 
+	  	  case 7:stmt.setString(2,"ECONOMICS");
+	  	         res=getSubjectTopper(stmt,rs);break; 
+	  	  case 8:stmt.setString(2,"ENGLISH");
+	         	 res=getSubjectTopper(stmt,rs);break;
+	  	  case 9:stmt.setString(2,"HINDI");
+	  	         res=getSubjectTopper(stmt,rs);break;
+	   	  case 10:stmt.setString(2,"MATHS");
+	           	 res=getSubjectTopper(stmt,rs);
+	           	 res="valid";break;
+	      default: res="invalid";break;
+	  	  }
+		return res;  
+	  }
+	
+	private static String getSubjectTopper(PreparedStatement stmt, ResultSet rs) throws SQLException {
+		String res="";
+		rs = stmt.executeQuery();
+		return res=getSubjectTopperClass(rs);
+	}
+	
+	private static String getSubjectTopperClass(ResultSet rs) throws SQLException {
+		String res="";
+		System.out.println("The topper in the subject  is:");
+		while(rs.next()) {
+			    System.out.print("Subject name: "+rs.getString("subjectName")+", Student name: "+rs.getString("name")+", Marks: "+rs.getInt("marks"));
+			    System.out.println();
+			    int r1=rs.getInt("marks");
+			    res=rs.getString("subjectName")+"-"+rs.getString("name")+"-"+r1;
+			    break;
+			}
+		return res;		
+		}
+
+
+}
+
 	public static String subjectTopper(PreparedStatement stmt, int ch) throws SQLException {
 		String res="";
 		ResultSet rs=null;
