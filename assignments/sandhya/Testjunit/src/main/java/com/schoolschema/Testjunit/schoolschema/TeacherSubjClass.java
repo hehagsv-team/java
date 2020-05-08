81d2bad6c5f9001888abfde3b7ab3871ba7f7d8d
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-08 14:14:39 +0530

    Add files via upload

---
+++ assignments/sandhya/Testjunit/src/main/java/com/schoolschema/Testjunit/schoolschema/TeacherSubjClass.java
@@ -0,0 +1,96 @@
+package com.schoolschema.Testjunit.schoolschema;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class TeacherSubjClass {
+	static Logger log=Logger.getLogger("TeacherSubjClass");
+	static String sql;
+	public static String teacherSubject() {
+		return sql = "select class, Average,teacher from(\r\n" + 
+				"select s.class_name as class,avg(marks) as Average,s.name ,t.name as teacher,dense_rank() over( partition by s.name order by avg(marks) desc) top\r\n" + 
+				"from marks m \r\n" + 
+				" join subject s \r\n" + 
+				"on m.subject_id=s.id \r\n" + 
+				"join teacher t\r\n" + 
+				"on s.teacher_id=t.id\r\n" +
+				"where s.name=?"+
+				"group by s.class_name,s.name,t.name\r\n" + 
+				")a where top=1";
+	}
+
+	public static  String teacherSubjMethod(Scanner sc, Connection conn) throws SQLException {
+		String res="";
+		while(true) {
+  	      log.info("\nWhich teacher teaches a subject better");
+      	  Menu.subjectMenu();
+      	  System.out.println("12: To return main menu:");
+      	  int ch=sc.nextInt();
+      	  sql=teacherSubject(); 
+      	  ResultSet rs = null;
+  	      PreparedStatement stmt=conn.prepareStatement(sql);
+  	      getTeacherSubjMethod(ch,stmt);
+ 		  if(ch==12) {
+ 			  res="exit";
+ 			  break;
+ 		     }
+           }
+		return res;
+	}
+	public static String getTeacherSubjMethod(int ch, PreparedStatement stmt) throws SQLException {
+		String res="";
+		ResultSet rs=null;
+		switch(ch) {
+	   	  case 1:stmt.setString(1,"PHYSICS");
+	       	     res=subTeacherMethod(rs,stmt);break;
+	   	  case 2:stmt.setString(1,"CHEMISTRY");         		  
+	   	         res=subTeacherMethod(rs,stmt);break;
+	   	  case 3:stmt.setString(1,"BIOLOGY");
+		         res=subTeacherMethod(rs,stmt);break;
+	   	  case 4:stmt.setString(1,"HISTORY");
+	             res=subTeacherMethod(rs,stmt);break;
+	   	  case 5:stmt.setString(1,"GEOGRAPHY");
+	             res=subTeacherMethod(rs,stmt);break;
+	   	  case 6:stmt.setString(1,"POLITICAL SCIENCE");
+	             res=subTeacherMethod(rs,stmt);break;
+	   	  case 7:stmt.setString(1,"ECONOMICS");
+	             res=subTeacherMethod(rs,stmt);break;
+	   	  case 8:stmt.setString(1,"ENGLISH");
+	             res=subTeacherMethod(rs,stmt);break;
+	   	  case 9:stmt.setString(1,"HINDI");
+	             res=subTeacherMethod(rs,stmt);break;
+	   	  case 10:stmt.setString(1,"MATHS");
+	              res=subTeacherMethod(rs,stmt);
+	              res="valid";break;
+	        default:res="invalid";break;
+			  }
+		return res;
+		}
+	private static String subTeacherMethod(ResultSet rs, PreparedStatement stmt) throws SQLException {
+		String res="";
+		 rs = stmt.executeQuery();
+		 return res=getTeacherSubject(rs);
+		
+	}
+
+	private static String getTeacherSubject(ResultSet rs) throws SQLException {
+		log.info("The teacher who teaches the subject better is\n");	
+		String res="";
+	    while(rs.next()) {
+	          log.debug("Class:"+rs.getInt("class")+","+"Average:"+rs.getInt("Average")+","+"Teachers-name: "+rs.getString("teacher"));
+	          int r1=rs.getInt("class");
+	          int r2=rs.getInt("Average");
+	       	  res=r1+"-"+r2+"-"+rs.getString("teacher");
+	       	  break;
+	       }
+		return res; 
+		}
+
+
+}
---
+++ assignments/sandhya/Testjunit/src/test/java/com/schoolschema/Testjunit/schoolschema/StudentTopperClassTest.java
@@ -0,0 +1,98 @@
+package com.schoolschema.Testjunit.schoolschema;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class StudentTopperClassTest {
+	
+@Mock
+private Connection conn;
+@Mock
+private PreparedStatement stmt;
+@Mock
+private ResultSet rs;
+
+@Before
+public void beforeEachTest_Method() throws Exception {
+	
+	MockitoAnnotations.initMocks(this);
+	when(conn.prepareStatement(any(String.class))).thenReturn(stmt);
+	when(stmt.executeQuery()).thenReturn(rs);	
+}
+@Test
+public void subStudentMethod_Case1() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	int ch=1;
+	when(rs.next()).thenReturn(true);
+	when(rs.getString("StudentName")).thenReturn("Anvar");
+	when(rs.getInt("marks")).thenReturn(1185);
+	String result=StudentTopperClass.subStudentMethod(stmt, ch);
+	assertEquals("Anvar-1185",result);
+}
+@Test
+public void subStudentMethod_Case2() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	int ch=2;
+	when(rs.next()).thenReturn(true);
+	when(rs.getString("StudentName")).thenReturn("Anu");
+	when(rs.getInt("marks")).thenReturn(1010);
+	String result=StudentTopperClass.subStudentMethod(stmt, ch);
+	assertEquals("Anu-1010",result);
+}
+@Test
+public void subStudentMethod_Case3() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	int ch=3;
+	when(rs.next()).thenReturn(true);
+	when(rs.getString("StudentName")).thenReturn("Gowthu");
+	when(rs.getInt("marks")).thenReturn(900);
+	String result=StudentTopperClass.subStudentMethod(stmt, ch);
+	assertEquals("Gowthu-900",result);
+}
+@Test
+public void subStudentMethod_Case4() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	int ch=4;
+	when(rs.next()).thenReturn(true);
+	when(rs.getString("StudentName")).thenReturn("Harika");
+	when(rs.getInt("marks")).thenReturn(1085);
+	String result=StudentTopperClass.subStudentMethod(stmt, ch);
+	assertEquals("Harika-1085",result);
+}
+@Test
+public void invalidBoundaryTest() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	int ch=0;
+	String result=StudentTopperClass.subStudentMethod(stmt, ch);
+	assertEquals("invalid",result);
+	}
+
+@Test
+public void validBoundaryTest() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	int ch=5;
+	String result=StudentTopperClass.subStudentMethod(stmt, ch);
+	assertEquals("valid",result);
+	}
+@Test
+public void testWhileExit() throws Exception{
+	StudentTopperClass stc=new StudentTopperClass();
+	Scanner sc=new Scanner("12");
+	String result=StudentTopperClass.studentTopperMethod(sc,conn);
+	assertEquals("exit",result);
+}
+
+}
+
