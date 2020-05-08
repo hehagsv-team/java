b9cfd776dbe85156e84fd7318f889d4efd96a71e
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-08 07:43:25 +0530

    Add files via upload

---
+++ assignments/sandhya/src/main/java/com/log4j/jdbc/schoolschema/TeacherClass.java
@@ -0,0 +1,64 @@
+package com.log4j.jdbc.schoolschema;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class TeacherClass {
+	static Logger log=Logger.getLogger("TeacherClass");
+	public static String teacherClass() {
+	     String sql;
+		return sql = "select t.name\r\n" + 
+	 	 		"from class c\r\n" + 
+	 	 		"join teacher t\r\n" + 
+	 	 		"on t.id=c.teacher_id\r\n" + 
+	 	 		"where c.name=?";
+	}
+
+	public static void teacherClassMethod(Scanner sc, Connection conn) throws SQLException {
+	while(true) {
+  	  log.info("\nName of the teacher in  particular class is: ");
+	  Menu.classMenu();
+	  ResultSet rs = null;
+  	  int ch=sc.nextInt();
+  	  String sql=teacherClass(); 
+  	  PreparedStatement stmt=conn.prepareStatement(sql);
+	  subTeacherClassMethod(ch,stmt,rs); 
+		  if(ch==12) {
+			  break;
+		   }
+       }
+	}
+
+private static void subTeacherClassMethod(int ch, PreparedStatement stmt, ResultSet rs) throws SQLException {
+	switch(ch) {
+	  case 1: stmt.setInt(1,6);
+              rs = stmt.executeQuery();
+              getTeacherClass(rs,6); break;
+	  case 2: stmt.setInt(1,7);
+              rs = stmt.executeQuery();
+              getTeacherClass(rs,7); break;
+	  case 3: stmt.setInt(1,8);
+              rs = stmt.executeQuery();
+              getTeacherClass(rs,8); break;
+	  case 4: stmt.setInt(1,9);
+              rs = stmt.executeQuery();
+              getTeacherClass(rs,9); break;
+	  case 5: stmt.setInt(1,10);
+              rs = stmt.executeQuery();
+              getTeacherClass(rs,10); break;
+     default:  break;  
+	  }
+}
+
+private static void getTeacherClass(ResultSet rs, int i) throws SQLException {
+	while(rs.next()) {
+	    log.debug("The name of the teacher in the particular class "+i+" is: "+rs.getString("name")+"\n");
+	}
+
+}	
+}
