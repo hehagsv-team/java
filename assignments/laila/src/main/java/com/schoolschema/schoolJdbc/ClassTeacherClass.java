f0daa08ec85dd72ae0b12134cc45feb9fc1d7062
LailaPasala <64887267+LailaPasala@users.noreply.github.com>
2020-05-09 09:11:31 +0530

    Update ClassTeacherClass.java

--- assignments/laila/src/main/java/com/schoolschema/schoolJdbc/ClassTeacherClass.java
+++ assignments/laila/src/main/java/com/schoolschema/schoolJdbc/ClassTeacherClass.java
@@ -1,5 +1,3 @@
-
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,7 +16,8 @@ public class ClassTeacherClass {
 	 	 	    "where c.name=?\r\n"+
 	 	 		"group by t.name,s.name\r\n";
 	}
-	public static void classTeacherMethod(Scanner sc, Connection conn) throws SQLException {
+	public static String classTeacherMethod(Scanner sc, Connection conn) throws SQLException {
+		String res="";
 	      while(true) {
 	      System.out.println("\nWhich all subjects does a class teacher teach is");
 		  Menu.classMenu();
@@ -26,38 +25,49 @@ public class ClassTeacherClass {
 		  sql=classTeacherSub();
 		  ResultSet rs=null;
 	      PreparedStatement stmt=conn.prepareStatement(sql);
-		  subClassTeacher(ch,stmt,rs);
+		  subClassTeacher(ch,stmt);
 		  if(ch==12) {
+			  res="exit";
 			  break;
 		    }
 	      }
+		return res;
 	}
-	private static void subClassTeacher(int ch, PreparedStatement stmt, ResultSet rs) throws SQLException {
+	public static String subClassTeacher(int ch, PreparedStatement stmt) throws SQLException {
+		String res="";
+		ResultSet rs=null;
 		 switch(ch) {
 		 case 1:stmt.setInt(1,6);
 	          rs = stmt.executeQuery();
-	          getClassTeacher(rs,6);  break;
+	          res=getClassTeacher(rs,6);  break;
 		  case 2:stmt.setInt(1,7);
 	          rs = stmt.executeQuery();
-	          getClassTeacher(rs,7);  break;
+	          res=getClassTeacher(rs,7);  break;
 		  case 3:stmt.setInt(1,8);
 	          rs = stmt.executeQuery();
-	          getClassTeacher(rs,8);  break;
+	          res=getClassTeacher(rs,8);  break;
 		  case 4:stmt.setInt(1,9);
 	          rs = stmt.executeQuery();
-	          getClassTeacher(rs,9);  break;
+	          res=getClassTeacher(rs,9);  break;
 		  case 5:stmt.setInt(1,10);
 	          rs = stmt.executeQuery();
-	          getClassTeacher(rs,10);
-	          System.out.println("null");  break;
-	      default:  break;
+	          res=getClassTeacher(rs,10);
+	          System.out.println("null"); 
+	          res="valid";break;
+	      default: res="invalid"; break;
 		  }
+		 return res;
 	}
-	private static void getClassTeacher(ResultSet rs, int i) throws SQLException {
+	private static String getClassTeacher(ResultSet rs, int i) throws SQLException {
 		System.out.println("The class teacher of the class name "+i+" is:");
+		String res="";
 		while(rs.next()) {
 			    System.out.print("Classteacher name: "+rs.getString("name")+", Subject name: "+rs.getString("no_subject"));
 			    System.out.println();
+			    res=rs.getString("name")+"-"+rs.getString("no_subject");
+			    break;
 			}
+		
+		return res;
 		}
 }
