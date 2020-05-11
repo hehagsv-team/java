3a8b434a7c7cf42d10870be9d0270f00d2f15151
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-11 15:45:22 +0530

    Update MyAnnotation.java

--- assignments/sandhya/MyAnnotation.java
+++ assignments/sandhya/MyAnnotation.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@interface MpsTrainee  // interface is used to create an annotation
+@interface MpsTrainee  
 {
 	String name();
 	int num();
