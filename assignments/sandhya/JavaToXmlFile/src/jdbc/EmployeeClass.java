4c3c62e096aebfc00483b88887d4fef3ef13bb18
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-13 23:53:16 +0530

    Add files via upload

---
+++ assignments/sandhya/JavaToXmlFile/src/jdbc/EmployeeClass.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+public class EmployeeClass {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
+
