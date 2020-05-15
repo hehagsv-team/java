1b95681301dd600236cf96e96bab07cdb668436d
RoddamHarika <57518391+RoddamHarika@users.noreply.github.com>
2020-05-15 12:53:59 +0530

    Add files via upload

---
+++ assignments/harika/sampleinvokemethods/src/invokemethods/ReflectionClass.java
@@ -0,0 +1,30 @@
+package invokemethods;
+
+public class ReflectionClass {
+//    private field
+    /** javadoc (ignored). */
+    private String s;
+
+//    public method with no arguments
+    /** javadoc (ignored). */
+    public final void method1() {
+        System.out.println("The string is " + s);
+    }
+
+//    public method with int as argument
+    /**
+     * javadoc (ignored).
+     *
+     * @param n
+     *
+     */
+    public final void method2(final int n) {
+        System.out.println("The number is " + n);
+    }
+
+//    private method
+    private void method3() {
+        System.out.println("Private method invoked");
+    }
+}
+
