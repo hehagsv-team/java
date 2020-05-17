867fff69367038d2f145daa7de01582a941e5ec6
SanSuresh <57612836+SanSuresh@users.noreply.github.com>
2020-05-17 23:09:33 +0530

    Add files via upload

---
+++ assignments/sandhya/JavaToXmlFile/src/jdbc/ExampleMethodClass.java
@@ -0,0 +1,11 @@
+package jdbc;
+
+public class ExampleMethodClass {
+    private static void addition(int a, int b) {
+        System.out.println("This is the addition method :" + (a + b));
+    }
+
+    private static void subtraction(int a, int b) {
+        System.out.println("This is the subtraction method :" + (a - b));
+    }
+}
--- assignments/sandhya/JavaToXmlFile/src/jdbc/JavaToXml.java
+++ assignments/sandhya/JavaToXmlFile/src/jdbc/JavaToXml.java
@@ -1,35 +1,85 @@
 package jdbc;
 
-import java.beans.XMLEncoder;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
+import java.lang.reflect.Method;
 
-public class JavaToXml {
-    public static void main(String[] args) throws FileNotFoundException {
-        EmployeeClass obj = new EmployeeClass();
-        obj.setFirstName("Hemadri");
-        obj.setLastName("teruvai");
-        obj.setAge(21);
-        FileOutputStream out = new FileOutputStream("C:\\log/outputFile.xml");
-        BufferedOutputStream os = new BufferedOutputStream(out);
-        XMLEncoder xml = new XMLEncoder(os);
-        xml.writeObject(obj);
-        xml.close();
-        BufferedReader in = new BufferedReader(new FileReader("C:\\log/outputFile.xml"));
-        String line;
-        try {
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
-            in.close();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public final class JavaToXml {
+    private JavaToXml() {
+        throw new IllegalStateException("Utility class");
+    }
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args)
+            throws ParserConfigurationException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.newDocument();
+        ExampleMethodClass obj = new ExampleMethodClass(); // Class
+        Class c = obj.getClass();
+        Method[] allMethods = c.getDeclaredMethods();
+        javaToXml(c, allMethods, doc);
     }
 
+    private static void javaToXml(final Class c, final Method[] allMethods,
+            final Document doc) {
+        Element rootElement = doc.createElement("class");
+        doc.appendChild(rootElement);
+        Attr attr = doc.createAttribute("name");
+        attr.setValue(c.getName());
+        rootElement.setAttributeNode(attr);
+        for (Method classMethod : allMethods) {
+            Class<?>[] pType = classMethod.getParameterTypes();
+            Element method = doc.createElement("method");
+            rootElement.appendChild(method);
+            Attr methodAttr = doc.createAttribute("name");
+            methodAttr.setValue(classMethod.getName());
+            method.setAttributeNode(methodAttr);
+            Element args = doc.createElement("args");
+            method.appendChild(args);
+            for (Class classobject : pType) {
+                Element arg = doc.createElement("arg");
+                args.appendChild(arg);
+                Element type = doc.createElement("type");
+                type.appendChild(doc.createTextNode(classobject.getName()));
+                arg.appendChild(type);
+            }
+        }
+        transformToXml(doc);
+    }
+
+    private static void transformToXml(final Document doc) {
+        try {
+            TransformerFactory transformerFactory =
+                    TransformerFactory.newInstance();
+            Transformer transformer;
+            transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(
+                    new File("C:\\log/outputFile.xml"));
+            transformer.transform(source, result);
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
 }
